055bfd7fe388ce652f9e37e152b98dfffcfb1b14
DarkSide20042 <135182785+DarkSide20042@users.noreply.github.com>
2023-05-31 23:59:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 
+
+
+int main (void){
+ setlocale(LC_ALL, "Portuguese");
+int tecla;
+
+switch(tecla){
+case 1:
+  printf ("intru��o 1");
+break;
+case 2:
+  printf ("intru��o 2");
+break;
+
+default:
+  printf ("intru��o 3");
+}
+return 0;
+}
