905afc8df5b17b56f79ff879255264befdda0fc1
DarkSide20042 <135182785+DarkSide20042@users.noreply.github.com>
2023-05-31 23:57:30 -0300

    Update funções.c

--- funções.c
+++ funções.c
@@ -1,1 +1,17 @@
 
+
+//fun��es
+typedef  struct lista
+{
+    int dado;
+    struct No *esquerda;
+    struct No *direita;
+}lista;
+
+typedef  struct tree
+{
+    int dado;
+    struct No *esquerda;
+    struct No *direita;
+}Arvore;
+
