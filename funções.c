041b7fb66c5a176b0d1b1b21ee2c9a031dd2af52
DarkSide20042 <135182785+DarkSide20042@users.noreply.github.com>
2023-06-23 23:37:09 -0300

    Update funções.c
    funções da arvore

--- funções.c
+++ funções.c
@@ -1,17 +1,18 @@
+// nó da árvore
+typedef struct No {
+    int x;
+    char historia[300];
+    struct No* esquerda;
+    struct No* direita;
+} No;
 
-
-//fun��es
-typedef  struct lista
-{
-    int dado;
-    struct No *esquerda;
-    struct No *direita;
-}lista;
-
-typedef  struct tree
-{
-    int dado;
-    struct No *esquerda;
-    struct No *direita;
-}Arvore;
-
+// Função para criar um novo nó
+No* criarNo(int x, char historia[300]) {
+    No* novoNo = (No*)malloc(sizeof(No));
+    novoNo->x = x;
+    strcpy(novoNo->historia, historia);
+    novoNo->esquerda = NULL;
+    novoNo->direita = NULL;
+    //return novoNo permite q eu consiga acessar o endereço de memoria do nó recem criado assim conseguindo usa-lo na arvore
+    return novoNo;
+}
