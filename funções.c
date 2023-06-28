7d32c04a41911356d78fdfdfdea6e35846fc17f4
DarkSide20042 <135182785+DarkSide20042@users.noreply.github.com>
2023-06-28 15:16:37 -0300

    Update funções.c

--- funções.c
+++ funções.c
@@ -1,18 +1,75 @@
-// nÛ da ·rvore
-typedef struct No {
+//estrutura da arvore
+typedef struct No
+{
     int x;
-    char historia[300];
+    char historia[2000];
     struct No* esquerda;
     struct No* direita;
 } No;
-
-// FunÁ„o para criar um novo nÛ
-No* criarNo(int x, char historia[300]) {
+//fun√ß√£o cria n√≥
+No* criarNo(int x, const char* historia)
+{
     No* novoNo = (No*)malloc(sizeof(No));
     novoNo->x = x;
     strcpy(novoNo->historia, historia);
     novoNo->esquerda = NULL;
     novoNo->direita = NULL;
-    //return novoNo permite q eu consiga acessar o endereÁo de memoria do nÛ recem criado assim conseguindo usa-lo na arvore
     return novoNo;
+}
+//fun√ß√£o para navegar na arvore atrasvez de sub arvores
+void navegarArvore(No* raiz)
+{
+    int escolha;
+
+    while (raiz != NULL)
+    {
+        printf("%s\n", raiz->historia);
+        printf("(3) Sair da hist√≥ria\n");
+        scanf("%d", &escolha);
+
+        switch (escolha)
+        {
+        case 1:
+            if (raiz->esquerda == NULL)
+            {
+                printf("Caminho inexistente\n");
+            }
+            else
+            {
+                raiz = raiz->esquerda;
+            }
+            break;
+        case 2:
+            if (raiz->direita == NULL)
+            {
+                printf("Caminho inexistente\n");
+            }
+            else
+            {
+                raiz = raiz->direita;
+            }
+            break;
+        case 3:
+            printf(". . .Saindo no Jogo. . .");
+            return;
+        default:
+            printf("Caminho inv√°lido\n");
+        }
+    }
+}
+//fun√ß√£o para salvar em arquivos o caminho feito pelo usuario
+void salvarCaminho(const char* aventura)
+{
+    FILE* arquivo = fopen("aventura.txt", "a");
+    if (arquivo == NULL)
+    {
+        printf("Erro ao criar o arquivo\n");
+        return;
+    }
+
+    fprintf(arquivo, "Caminho percorrido:\n");
+    fprintf(arquivo, "%s", aventura);
+
+    fclose(arquivo);
+    printf("\nCaminho salvo no arquivo 'aventura.txt'\n");
 }
